ab823f97ea198340107733768b8ca00866caef63
NachoCano123 <145673920+NachoCano123@users.noreply.github.com>
2023-11-09 13:09:54 +0100

    Update main.ts

--- mongodb/main.ts
+++ mongodb/main.ts
@@ -1,6 +1,6 @@
 import express,{Request, Response} from "npm:express@4.18.2" //https://www.npmjs.com/package/express
 import mongoose from "npm:mongoose@7.6.3"
-await mongoose.connect("mongodb+srv://nacho:12345@cluster0.mttqytn.mongodb.net/pruebadatabase?retryWrites=true&w=majority")
+await mongoose.connect("mongodb+srv://nacho:12345@cluster0.3aayvs9.mongodb.net/pruebadatabase?retryWrites=true&w=majority")
 const miapp = express()
 
 miapp.listen(3000, () =>
